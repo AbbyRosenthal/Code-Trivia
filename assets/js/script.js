07ebe5cd4e171fc2a80d3ff6a9668a45d50eb0c8
Abby Rosenthal <adlundeen@gmail.com>
2022-05-23 13:51:12 -0500

    div containers for questions, answer, timer and high score

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+var myQuestions = [
+    {
+        question: "Which would change the styling of a webpage?",
+        answers: {
+            a: "css",
+            b: "html",
+            c: "javascript",
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "INSERT QUESTION",
+        answers: {
+            a: "OPTION",
+            b: "OPTION",
+            c: "OPTION",
+        },
+        correctAnswer: 'c'
+    },
+]
