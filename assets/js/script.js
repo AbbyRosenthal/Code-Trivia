4c10e58ed3b6dbfb6e6bd0e4b2709e306465e53a
Abby Rosenthal <adlundeen@gmail.com>
2022-05-24 13:58:18 -0500

    attmepting to make a countdowntimer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var timerEl = document.getElementById('timer');
+
 var myQuestions = [
     {
         question: "Which would change the styling of a webpage?",
@@ -18,3 +20,24 @@ var myQuestions = [
         correctAnswer: 'c'
     },
 ]
+
+//countdown timer in header
+function countdown() {
+    var timeLeft = 30;
+
+    var timeInterval = setInterval (function() {
+        if(timeLeft > 1) {
+            timerEl.textContent = timeLeft + ' seconds remaining ';
+            timeLeft--;
+        }
+else if (timeLeft === 1) {
+    timerEl.textContent = timeLeft + ' second remaining';
+    timeLeft--;
+}
+else {
+    timerEl.textContent = '';
+    clearInterval(timeInterval);
+    }
+}, 1000);
+}
+countdown();
