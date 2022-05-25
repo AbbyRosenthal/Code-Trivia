5e0a3728f4ed3b27c923a9bd2879f47f20c40f68
Abby Rosenthal <adlundeen@gmail.com>
2022-05-24 19:11:00 -0500

    added questions to the variable in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,33 @@ var myQuestions = [
         },
         correctAnswer: 'c'
     },
+    {
+        question: "INSERT QUESTION",
+        answers: {
+            a: "OPTION",
+            b: "OPTION",
+            c: "OPTION",
+        },
+        correctAnswer: 'c'
+    }, 
+    {
+        question: "INSERT QUESTION",
+        answers: {
+            a: "OPTION",
+            b: "OPTION",
+            c: "OPTION",
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "INSERT QUESTION",
+        answers: {
+            a: "OPTION",
+            b: "OPTION",
+            c: "OPTION",
+        },
+        correctAnswer: 'c'
+    },
 ]
 
 //countdown timer in header
@@ -27,19 +54,19 @@ function countdown() {
 
     //based on time in class activity **IT WORKS!!!!
     //HOW TO DEDUCT TIME BASED ON WRONG ANSWERS!!!
-    var timeInterval = setInterval (function() {
-        if(timeLeft > 1) {
+    var timeInterval = setInterval(function () {
+        if (timeLeft > 1) {
             timerEl.textContent = timeLeft + ' seconds remaining ';
             timeLeft--;
         }
-else if (timeLeft === 1) {
-    timerEl.textContent = timeLeft + ' second remaining';
-    timeLeft--;
-}
-else {
-    timerEl.textContent = '';
-    clearInterval(timeInterval);
-    }
-}, 1000);
+        else if (timeLeft === 1) {
+            timerEl.textContent = timeLeft + ' second remaining';
+            timeLeft--;
+        }
+        else {
+            timerEl.textContent = '';
+            clearInterval(timeInterval);
+        }
+    }, 1000);
 }
 countdown();
