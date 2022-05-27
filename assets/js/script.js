3d3092ebff57cd6b9b073f0dd012a563cc848aa7
Abby Rosenthal <adlundeen@gmail.com>
2022-05-26 19:03:25 -0500

    fixed broken timer and displayed questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,67 @@
 var timerEl = document.getElementById('countdown');
-let questionsEl = document.getElementById('questionList')
-let answersEl = document.getElementById('answerList')
+var questionsEl = document.querySelector('#questionList');
+let answersEl = document.getElementById('answerList');
+let startButtonEl = document.getElementById('start-btn');
 
-//variables to manipulate numbers on page
-var clock = 60;
-var score = 0;
-var penalty = 3;
 
 //questions array for quiz questions
-var questionsArray = [
+var questions = [
     {
         question: "Which would change the styling of a webpage?",
-        answer:["css, javascript, html, good luck"],
-        correctAnswer: 0,
+        answer: {
+            a: "css",
+            b: "javascript",
+            c: "html",
+        },
+        correctAnswer: 'a'
     },
     {
-        question: "INSERT QUESTION",
-        answers:["1, 2, 3, 4"],
-        correctAnswer: 2,
+        question: "Which would change the styling of a webpage?",
+        answer: {
+            a: "css",
+            b: "javascript",
+            c: "html",
+        },
+        correctAnswer: 'a'
     },
     {
-        question: "INSERT QUESTION",
-        answers:["1, 2, 3, 4"],
-        correctAnswer: 2,
+        question: "Which would change the styling of a webpage?",
+        answer: {
+            a: "css",
+            b: "javascript",
+            c: "html",
+        },
+        correctAnswer: 'a'
     },
     {
-        question: "INSERT QUESTION",
-        answers:["1, 2, 3, 4"],
-        correctAnswer: 2,
+        question: "Which would change the styling of a webpage?",
+        answer: {
+            a: "css",
+            b: "javascript",
+            c: "html",
+        },
+        correctAnswer: 'a'
     },
     {
-        question: "INSERT QUESTION",
-        answers:["1, 2, 3, 4"],
-        correctAnswer: 2,
+        question: "Which would change the styling of a webpage?",
+        answer: {
+            a: "css",
+            b: "javascript",
+            c: "html",
+        },
+        correctAnswer: 'a'
     },
 ]
 
-//countdown timer in header
+// event listeners for start button to generate questions and timer
+
+startButtonEl.addEventListener("click", beginQuiz)
+startButtonEl.addEventListener("click", countdown)
+
+//TIMER - is janky and needs to be fixed since adding event listener
 function countdown() {
     var timeLeft = 30;
-
-    //based on time in class activity **IT WORKS!!!!
+    //based on time in class activity 
     //HOW TO DEDUCT TIME BASED ON WRONG ANSWERS!!!
     var timeInterval = setInterval(function () {
         if (timeLeft > 1) {
@@ -57,25 +78,29 @@ function countdown() {
         }
     }, 1000);
 }
-countdown();
 
-//FUNCTION TO START GAME
-
+//FUNCTION TO BEGIN THE QUIZ
 function beginQuiz() {
-var userInitials = prompt("Welcome to Code Triva!  Please enter your initials to begin!")
+    var userInitials = prompt("Welcome to Code Triva!  Please enter your initials to begin!")
     console.log(userInitials);
-}
- beginQuiz ();
+    insertQuestions()
 
+};
+
+// beginQuiz();
+
+function insertQuestions() {
+    questionsEl.textContent = questions[1].question
+    for (var i = 0; i < questions.length; i++) {
+        var answerOptions = document.createElement("button")
+        answerOptions.textContent = questions.answer.a;
+        answerOptions.setAttribute("value", questions[i]);
+        answersEl.appendChild(answerOptions);
+        //append to select
+
+    }
+}
 
 //FUNTION TO SELECT QUESTIONS USING .MAP
-function selectAnswer() {
-    for (var i=0; i <questionsArray.length; i++) {
-        questionsEl = questionsArray.push
-    }
-
-}
-selectAnswer();
 
 //FUNCTION TO END GAME
-
